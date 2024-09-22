95b0d947abee0d4e7acfcaab5095c41cd5b7665f
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-09-21 22:03:03 -0400

    Haha

--- generate_docs.bat
+++ generate_docs.bat
@@ -1,5 +1,8 @@
 @echo off
 echo This also builds the game for you.
+echo ONLY USE THIS IF YOU HAVE HAXE INSTALLED ON YOUR SYSTEM!
+haxelib install dox
+haxelib install hxargs
 lime build windows -xml
 haxelib run dox -o bin/api -i bin/windows/types.xml -in music -in elements -in utils -in system -in menus
 cd ./bin/api
