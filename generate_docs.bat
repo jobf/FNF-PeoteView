63b617b4e4fa19d08ffe73cba9edcbf8d9361fe8
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2024-09-27 22:20:59 -0400

    Update generate_docs.bat

--- generate_docs.bat
+++ generate_docs.bat
@@ -6,7 +6,8 @@ timeout /t 1 /nobreak >nul
 haxelib install dox
 haxelib install hxargs
 lime build windows -xml -dce no
-haxelib run dox -o bin/api -i bin/windows/types.xml -in system -in elements -in utils -in "music/Audio" -in "music/Conductor" -in "music/chart"
+haxelib run dox -o bin/api -i bin/windows/types.xml -in system -in elements -in utils -in music
 cd ./bin/api
 index.html
+cls
 echo Done!
