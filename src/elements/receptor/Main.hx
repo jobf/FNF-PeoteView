721b69f5b715bdcbeff819e111beee6ec422cd5b
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2024-10-23 17:11:08 -0400

    Optimize `ReceptorAndSteps`

--- src/elements/receptor/Main.hx
+++ src/elements/receptor/Main.hx
@@ -28,25 +28,53 @@ class Main extends Application
 	// --------------- SAMPLE STARTS HERE -------------------------
 	// ------------------------------------------------------------	
 	var peoteView:PeoteView;
-	var playField:PlayField;
+	var display:Display;
+	var receptors:Array<ReceptorAndSteps> = [];
 	
 	public function startSample(window:Window)
 	{
 		peoteView = new PeoteView(window);
+		display = new Display(0, 0, window.width, window.height, Color.GREY2);
 
-		playField = new PlayField(peoteView,
-			window.width, window.height, Color.GREY2,
+		peoteView.addDisplay(display);
+
+		var angles = [0, -90, 90, 180];
+
+		if (false) {
+			var receptor = new ReceptorAndSteps(50, 50,
 			162, 164,
 			108, 110,
 			45, 35,
+			display,
 			"assets/notes/normal/receptor.png", "receptorTex",
 			"assets/notes/normal/note.png", "noteTex",
-			"assets/notes/normal/sustain.png", "sustainTex"
-		);
+			"assets/notes/normal/sustain.png", "sustainTex");
+			receptors.push(receptor);
+		} else {
+			for (j in 0...4) {
+				var receptor = new ReceptorAndSteps(50 + (112 * j) + 640, 50,
+				162, 164,
+				108, 110,
+				45, 35,
+				display,
+				"assets/notes/normal/receptor.png", "receptorTex",
+				"assets/notes/normal/note.png", "noteTex",
+				"assets/notes/normal/sustain.png", "sustainTex");
+				receptor.r = angles[j];
+				receptors.push(receptor);
+			}
+		}
 
 		//window.onKeyDown.add(Receptor.keyPress);
 		//window.onKeyUp.add(Receptor.keyRelease);
 
 		peoteView.start();
+	}
+
+	override function update(deltaTime:Int) {
+		for (i in 0...receptors.length) {
+			var receptor = receptors[i];
+			receptor.updateReceptor();
+		}
 	}
 }
--- src/elements/receptor/ReceptorAndSteps.hx
+++ src/elements/receptor/ReceptorAndSteps.hx
@@ -3,16 +3,16 @@ package elements.receptor;
 @:publicFields
 class ReceptorAndSteps {
     // This is where sustains will go
-    var bottomBuffer:Buffer<Sustain>;
-    var bottomProgram:Program;
+    private static var bottomBuffer(default, null):Buffer<Sustain>;
+    private static var bottomProgram(default, null):Program;
 
     // This is where the receptor will go
-    var middleBuffer:Buffer<Receptor>;
-    var middleProgram:Program;
+    private static var middleBuffer(default, null):Buffer<Receptor>;
+    private static var middleProgram(default, null):Program;
 
     // This is where steps will go
-    var topBuffer:Buffer<Note>;
-    var topProgram:Program;
+    private static var topBuffer(default, null):Buffer<Note>;
+    private static var topProgram(default, null):Program;
 
     var receptor:Receptor;
 
@@ -56,8 +56,6 @@ class ReceptorAndSteps {
         return receptor.scale = value;
     }
 
-    private static var textures(default, null):Map<String, Texture> = [];
-
     private var nW(default, null):Int;
     private var nH(default, null):Int;
     private var sW(default, null):Int;
@@ -78,67 +76,71 @@ class ReceptorAndSteps {
 
         // Receptor and note group setup
 
-        middleBuffer = new Buffer<Receptor>(1, 0, false);
-        middleProgram = new Program(middleBuffer);
-        middleProgram.blendEnabled = true;
+        if (middleBuffer == null) middleBuffer = new Buffer<Receptor>(1, 128, false);
 
-        if (!textures.exists(receptorName)) {
+        var middleIsNull = middleProgram == null;
+        if (middleIsNull) {
+            middleProgram = new Program(middleBuffer);
+            middleProgram.blendEnabled = true;
+
             var texture = new Texture(w * 3, h, null, {tilesX: 3, smoothExpand: true, smoothShrink: true, powerOfTwo: false});
 
             var textureBytes = sys.io.File.getBytes(receptorPath);
             var textureData = TextureData.fromFormatPNG(textureBytes);
 
             texture.setData(textureData);
-            textures[receptorName] = texture;
-        }
 
-		middleProgram.setTexture(textures[receptorName], receptorName, true);
+            middleProgram.setTexture(texture, receptorName, true);
+        }
 
         receptor = new Receptor(x, y, w, h, -27, -27);
         middleBuffer.addElement(receptor);
 
         // Note group setup
 
-        topBuffer = new Buffer<Note>(1, 1024, false);
-        topProgram = new Program(topBuffer);
-        topProgram.blendEnabled = true;
+        if (topBuffer == null) topBuffer = new Buffer<Note>(1, 2048, false);
 
-        if (!textures.exists(noteName)) {
+        var topIsNull = topProgram == null;
+        if (topIsNull) {
+            topProgram = new Program(topBuffer);
+            topProgram.blendEnabled = true;
+
             var texture = new Texture(nW, nH, null, {smoothExpand: true, smoothShrink: true, powerOfTwo: false});
 
             var textureBytes = sys.io.File.getBytes(notePath);
             var textureData = TextureData.fromFormatPNG(textureBytes);
 
             texture.setData(textureData);
-            textures[noteName] = texture;
+    
+            topProgram.setTexture(texture, noteName, true);
         }
-
-		topProgram.setTexture(textures[noteName], noteName, true);
 
         var note = new Note(x, y + 450, nW, nH);
         topBuffer.addElement(note);
 
         // Note group setup
 
-        bottomBuffer = new Buffer<Sustain>(1, 1024, false);
-        bottomProgram = new Program(bottomBuffer);
-        bottomProgram.blendEnabled = true;
+        if (bottomBuffer == null) bottomBuffer = new Buffer<Sustain>(1, 2048, false);
 
-        if (!textures.exists(sustainName)) {
+        var bottomIsNull = bottomProgram == null;
+        if (bottomIsNull) {
+            bottomProgram = new Program(bottomBuffer);
+            bottomProgram.blendEnabled = true;
+
             var texture = new Texture(sW, sH, null, {smoothExpand: true, smoothShrink: true, powerOfTwo: false});
 
             var textureBytes = sys.io.File.getBytes(sustainPath);
             var textureData = TextureData.fromFormatPNG(textureBytes);
 
             texture.setData(textureData);
-            textures[sustainName] = texture;
+
+            Sustain.init(display, bottomProgram, sustainName, texture);
         }
 
         // Do this to finalize this instance.
 
-		Sustain.init(display, bottomProgram, sustainName, textures[sustainName]);
-		display.addProgram(middleProgram);
-		display.addProgram(topProgram);
+		if (middleIsNull) display.addProgram(middleProgram);
+		if (topIsNull) display.addProgram(topProgram);
 
         var sustain = new Sustain(note.x + (nW >> 1), note.y + ((nH - sH) >> 1), sW, sH);
         sustain.tailPoint = 45;
