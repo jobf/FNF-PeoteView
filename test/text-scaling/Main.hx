2b373b548739b01cbc5d7fe857c28038173ed27c
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2024-12-28 11:25:53 -0500

    H

--- test/text-scaling/Main.hx
+++ test/text-scaling/Main.hx
@@ -5,6 +5,7 @@ import lime.app.Application;
 import lime.ui.Window;
 import lime.ui.KeyCode;
 import elements.Text;
+import system.*;
 
 @:publicFields
 class Main extends Application
@@ -12,10 +13,11 @@ class Main extends Application
 
 	override function onPreloadComplete() {	
 		var peoteView = new PeoteView(window);
+
+		TextureSystem.createTexture("vcrTex", "assets/fonts/vcrAtlas.png");
 		
 		var display = new Display(0, 0, window.width, window.height);
 		peoteView.addDisplay(display);
-
 		
 		var x:Float = 0;
 		var y:Float = 0;
@@ -25,6 +27,8 @@ class Main extends Application
 		var program = new Program(text.buffer);
 		program.blendEnabled = true;
 		display.addProgram(program);
+
+		TextureSystem.setTexture(program, 'vcrTex', 'vcrTex');
 		
 		text.text = 'testing....';
 
@@ -33,10 +37,7 @@ class Main extends Application
 			
 			frameCount += 1.0;
 
-			var sin = (Math.sin(frameCount / 200)  + 1) / 2;
-			trace(sin);
-
-			text.scale = (sin * 200);
+			text.scale += 0.01;
 		});
 
 	}
